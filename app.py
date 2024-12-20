2b56bb8f081bf6a0911b730b608f8ca9e8f0d888
Abhi G <abhijeet@mintix.no>
2024-12-21 00:03:50 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st;
+import pandas as pd;
+import numpy as np;
+from datetime import time, datetime;
+import string;
 
-st.title("Hello World")
+def hello_world():
+    st.title("Hello World")
+    st.chat_message("user")
+    st.write("Hello World again")
+    prompt = st.chat_input("Enter your prompt")
+    if prompt:
+        st.chat_message("assistant")
+        st.write(prompt)
+
+def main():
+    hello_world()
+
+if __name__ == "__main__":
+    main()
